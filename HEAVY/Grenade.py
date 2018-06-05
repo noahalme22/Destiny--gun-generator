d6848d6b5be81b458f7e7357ddc46aa470f269c6
noahalme22 <39957738+noahalme22@users.noreply.github.com>
2018-06-05 11:03:42 -0400

    stuffs

--- HEAVY/Grenade.py
+++ HEAVY/Grenade.py
@@ -2,15 +2,19 @@ import heavy,random
 
 class grandeLauncher(heavy.heavy):
         def __init__(self,grenadeLauncherList = ""):
+          super().__init__():
 
-        grenadeLauncherList = []
-        if grenadeaucherList = []
-              for i in range (0,3):
-            
-                grenadeLaucherList = random.choice(['Vacuum','Tripod','Third Eye','Surplus','Snapshot','Smart Drift Control',
-                                         'Single Point Sling','Quickdraw','Perfect Balence','Linear Compensator',
-                                         'Hard Launch','Flared Magwell','Fitted Stock','Field Scout','Army of One',
-                                         'Aggressive Launch'])
+          grenadeLauncherList = []
+          if grenadeLaucherList = []
+                for i in range (0,3):
+              
+                  grenadeLaucherList = random.choice(['Vacuum','Tripod','Third Eye','Surplus','Snapshot','Smart Drift Control',
+                                           'Single Point Sling','Quickdraw','Perfect Balence','Linear Compensator',
+                                           'Hard Launch','Flared Magwell','Fitted Stock','Field Scout','Army of One',
+                                           'Aggressive Launch'])
 
-	if grenade == "":
-		grenade = random.choice(["Play of the game","the colony","I Am Alive"])
+          if grenade == "":
+                  grenade = random.choice(["Play of the game","the colony","I Am Alive"])
+
+
+          
--- HEAVY/Rocket.py
+++ HEAVY/Rocket.py
@@ -1,7 +1,26 @@
 import random,heavy
 
 class rocketLauncher(heavy.heavy):
+        def __init__(self,grenadeLauncherList = ""):
+          super().__init__():
 
-	if rocket == "":
-				rocketRan = random.randint(1,6)
-				rocket = random.choice(["Gjallarhorn","Dragons Breath","Suros JLB-47","Curtain Call","Warcliff coil","Sins Of The Past"])
+          rocketLauncherList = []
+          rocketLaucherAttributes = []
+
+        if rocketLaucherAttributes = []
+                
+                rocket = random.choice('precision frame','LightWeight','Aggressive Frame','Adept Frame','High-Impact Frame','Rapid-Fire Frame')
+
+
+
+
+
+        if rocketLaucherList = []
+                for i in range (0,3):
+              
+                  LaucherList = random.choice(['Vacuum','Tripod','Third Eye','Surplus','Snapshot','Smart Drift Control',
+                                           'Single Point Sling','Quickdraw','Perfect Balence','Linear Compensator',
+                                           'Hard Launch','Flared Magwell','Fitted Stock','Field Scout','Army of One',
+                                           'Aggressive Launch'])
+        if rocket == "":
+                rocket = random.choice(["Gjallarhorn","Dragons Breath","Suros JLB-47","Curtain Call","Warcliff coil","Sins Of The Past"])
