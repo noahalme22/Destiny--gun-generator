c5033147eadcfcbbb2075f58077e8ba06ed011e2
noahalme22 <39957738+noahalme22@users.noreply.github.com>
2018-06-08 10:56:58 -0400

    sidearms and snipers

--- SECONDARY/SideArm.py
+++ SECONDARY/SideArm.py
@@ -1,8 +1,21 @@
 import random, secondary
 
 class sidearm(secondary.secondary)
+        def __init__(self, sidearnList = ""):
+        super().__init__():
 
-if sidearm == "":
-  sidearm = random.randint(1,6)
-  sidearm  = random.choice(["Wormwood","Tresspasser","Havoc Pigeon","Last Hope","The Last Dance","Rat King"])
+        sidearmList = []
+        sidearmAttributes = []
+        if sidearmAttributes = []
+                
+              sidearm = random.choice('precision frame','LightWeight','Aggressive Frame','Adept Frame','High-Impact Frame','Rapid-Fire Frame')
+
+        if sidearmList = []
+            for i in range (0,3):
+              
+                sidearmList = random.randint('Battle Runner','crowd control','full auto','outlaw',
+                                                    'quick draw','perfect balance','oiled frame','Send It','Single Point Sling')
+
+        if sidearm == "":
+              sidearm  = random.choice(["Wormwood","Tresspasser","Havoc Pigeon","Last Hope","The Last Dance","Rat King"])
 
--- SECONDARY/Sniper.py
+++ SECONDARY/Sniper.py
@@ -1,9 +1,26 @@
 import random, secondary
 
 class sniper(secondary.secondary)
+        def __init__(self,sniperList = ""):
+        super().__init__():
 
-if sniper == "":
-  sniper = random.randint(1,6)
-  sniper  = random.choice(["Ice Breaker","Longbow Synthesis","LDR 5001","D.A.R.C.I","Aachen-LR2","Borealis"])
+
+
+        sniperList = []
+        sniperAttributes = []
+        if sniperAttributes = []
+
+              sniper = random.choice('precision frame','LightWeight','Aggressive Frame','Adept Frame','High-Impact Frame','Rapid-Fire Frame')               
+
+
+
+        if sniperList = []
+            for i in range (0,3):
+                sniperList = random.randint('Third Eye','Take a Knee','Tacsys SLS15','Sightsys SLS20','Snapshot','Surplus','Spray and play','Rodeo','Replenish','Perfect Balence','Quickdraw','Outlaw','Mulligan','Lightweight','Injection Mold','Hidden Hand','Hand-Laid Stock','Hammer Forged','Flared Magwell','Final Round','Fitted Stock','Casket Mag','Braced Frame','Armor Piercing Rounds')
+
+
+
+        if sniper == "":
+              sniper  = random.choice(["Ice Breaker","Longbow Synthesis","LDR 5001","D.A.R.C.I","Aachen-LR2","Borealis"])
 
 
