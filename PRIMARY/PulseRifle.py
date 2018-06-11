db45090e4e58f5b6c52520680ced19080812887b
noahalme22 <39957738+noahalme22@users.noreply.github.com>
2018-06-11 10:07:40 -0400

    pulse rifle

--- PRIMARY/PulseRifle.py
+++ PRIMARY/PulseRifle.py
@@ -1,7 +1,26 @@
 import random,primary
 
 class PulseRifle(primary.primary):
+        def __init__(self,pulseRifleList = ""):
+        super().__init__():
+ 
+        pulseRifleList = []
+        pulseRifleAttributes = []
+        if pulseRifleAttributes = []
+
+                pulseRifle = random.choice('precision frame','LightWeight','Aggressive Frame','Adept Frame','High-Impact Frame','Rapid-Fire Frame')
+
+
+        if pulseRifleList = []
+                for i in range (0,3):
+                      pulseRifleList = random.randint('Armor Piercing Rounds','Braced Frame','Custom Optics','Firefly','Full Auto','Headseeker','Hidden Hand','Lightweight','Quickdraw','Quickdraw IS','Perfect Balence','Ranged Lens RLS3','Ranged Lens RLS5','Rangefinder','Red Dot-OAS','Red Dot-ORES','Red Dot-ORS','Spray and play','Steady Hand IS')
+
 
 	if pulseRifle == "":
-				pulseRan = random.randint(1,6)
-				pulseRifle = random.choice(["Grasp of Malok","Blind Predicion","vigilance wing","nightshade","red death","Graviton Lance"])
+                pulseRifle = random.choice(["Grasp of Malok","Blind Predicion","vigilance wing","nightshade","red death","Graviton Lance"])
+
+
+        
+
+
+        
