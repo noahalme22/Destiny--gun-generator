773953f05e9a7e6073848aee20b33fd0c62caa3c
John <holmesjw@gmail.com>
2018-06-11 10:56:36 -0400

    smg ready for testing

--- PRIMARY/AutoRifle.py
+++ PRIMARY/AutoRifle.py
@@ -4,7 +4,6 @@ import random,primary
 class AutoRifle(primary.primary):
 
 
-<<<<<<< HEAD
         def __init__(self,autoRifleList = :""):
         super().__init__():
 
@@ -12,27 +11,11 @@ class AutoRifle(primary.primary):
         
         if autoRilfeList == []:
                 for i in range (0,3):
-                        autoRifleRan = random.choice(['Armor Piercing Rounds','Army of One','Battle Runner','Braced Frame','Cascade','Crowd Control','Extended Mag','Fitted Stock','
-'Field Scout','Focused Fire','Hand Loaded','Hammer Forged','Hand-Laid Stock','Hidden Hand','Hip Fire','Hot Swap','Injection Mold','Icarus',
-'Lightweight','Outlaw','Quickdraw','Perfect Balence','Persistance','Rangefinder','Replenish','Single Point Sling','Smallbore',
-'Spray and play',''
+                        autoRifleRan = random.choice(['Armor Piercing Rounds','Army of One','Battle Runner','Braced Frame','Cascade','Crowd Control','Extended Mag','Fitted Stock','Field Scout','Focused Fire','Hand Loaded','Hammer Forged','Hand-Laid Stock','Hidden Hand','Hip Fire','Hot Swap','Injection Mold','Icarus','Lightweight','Outlaw','Quickdraw','Perfect Balence','Persistance','Rangefinder','Replenish','Single Point Sling','Smallbore','Spray and play','Snapshot','Third Eye'])
 
                 
-                
-=======
-        def __init__(self,):
-
-        super().__init__():
-
-
-
-
-
-
-
-
-
->>>>>>> 339797f6d2317df866b3ea7adcc96bf70682b0d5
+        if autoRifleAttributes == "":
+                autoRifleAttributes = random.choice(['precision frame','Lightweight Frame','Aggresive Frame','Adaptive Frame','High-Impact Frame','Rapid-Fire Frame'])         
 
 	if autoRifle == "":
                 autoRifle = random.choice(["Uriels Gift","doctrine of passing","Monte Carlo","origin Story","Zhalo Supercell","Kibou AR3"])
--- PRIMARY/HandCannon.py
+++ PRIMARY/HandCannon.py
@@ -2,8 +2,24 @@ import random,primary
 
 
 class HandCannon(primary.primary):
-
+        def __init__(self,hcList = "",hcAttributes == "",handCannon == ""):
+        super().__init__():
+        self.handCannon = handCannon
+        self.hcAttributes = hcAttributes
+        self.hcList = hcList
+        
 	if handCannon == "":
-				HCRan = random.randint(1,6)
-				handCannon = random.choice(["Crimson","Palindrome","Eyesluna","Last Word","Better Devils","Old Fashioned"])
-				
+                handCannon = random.choice(["Crimson","Palindrome","Eyesluna","Last Word","Better Devils","Old Fashioned"])
+
+	if hcAttributes == "":
+                hcAttributes = random.choice(['precision frame','Lightweight Frame','Aggresive Frame','Adaptive Frame','High-Impact Frame','Rapid-Fire Frame'])
+
+        hcList = []
+        
+        if hcList == []:
+                for i in range (0,3):
+                        hcRan = random.choice(['Army of One','Crowd Control','Field Scout','Final round','Firefly','Hand loaded','Hot Swap','Icarus','Lightweight','Outlaw','Quickdraw','Perfect Balence',
+'Luck in the Chamber','Rangefinder','Mulligan','Return to Sender','Send it','Single Point Sling','Smallbore','Spray and play','Snapshot','Third Eye'])
+
+        def __str__(self):
+                return super().__str__()+"\nhandCannon: "+str(self.handCannon)+"\nhcAttributes : "+str(self.hcAttributes)+"\nhcList: "+str(self.hcList)+"\n"
