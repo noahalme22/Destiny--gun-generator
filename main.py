ddfd13180411c7227c7d7c3eaab6364d8d4c6c3a
John <holmesjw@gmail.com>
2018-06-15 10:26:27 -0400

    LMAO

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 
-import PulseRifle as pr,HandCannon as hc, AutoRifle as ar, ScoutRifle as sr
+import Sniper as sr
 
-newWeapon = pr.PulseRifle ()
-newWeapon1 = hc.HandCannon ()
-newWeapon2 = ar.AutoRifle ()
-newWeapon3 = ar.ScoutRifle ()
+newWeapon = sr.Sniper ()
 print (newWeapon.gun)
-<<<<<<< HEAD
-sn = open (newWeapon.gun+".py",r
-=======
-print (newWeapon1.gun)
-print (newWeapon2.gun)
-print (newWeapon3.gun)
->>>>>>> 97f4698a7b8513b5875d144d22ac6e7710d0e823
+
