97f4698a7b8513b5875d144d22ac6e7710d0e823
noahalme22 <39957738+noahalme22@users.noreply.github.com>
2018-06-15 10:21:00 -0400

    stuffffffffs

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
-import PulseRifle as pr,HandCannon as hc
+import PulseRifle as pr,HandCannon as hc, AutoRifle as ar, ScoutRifle as sr
 
 newWeapon = pr.PulseRifle ()
-newWeapon1 = hc.handConnon ()
+newWeapon1 = hc.HandCannon ()
+newWeapon2 = ar.AutoRifle ()
+newWeapon3 = ar.ScoutRifle ()
 print (newWeapon.gun)
 print (newWeapon1.gun)
+print (newWeapon2.gun)
+print (newWeapon3.gun)
