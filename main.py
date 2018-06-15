838259ffba7b525d2ab41c0fa1a2fdf588d6d6cb
noahalme22 <39957738+noahalme22@users.noreply.github.com>
2018-06-15 14:15:16 -0400

    all weapons complete

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import Sniper as sr, SideArm as sa,ShotGun as sg,Fusion as fr
+import PulseRifle as pr, HandCannon as hc, AutoRifle as ar, ScoutRifle as scr, SMG as smg, Sword as sw, Grenade as gr, Rocket as ro, Machine as ma, Sniper as sr, SideArm as sa,ShotGun as sg,Fusion as fr
 
-<<<<<<< HEAD
-import PulseRifle as pr, HandCannon as hc, AutoRifle as ar, ScoutRifle as scr, SMG as smg, Sword as sw, Grenade as gr, Rocket as ro, Machine as ma
-=======
-<<<<<<< HEAD
-newWeapon7 = sr.Sniper ()
-newWeapon6 = sa.Sidearm ()
-newWeapon8 = sg.Shotgun ()
-newWeapon9 = fr.Fusion ()
-print (newWeapon7.gun)
-print (newWeapon6.gun)
-print (newWeapon8.gun)
-print (newWeapon9.gun)
-=======
-import PulseRifle as pr,HandCannon as hc, AutoRifle as ar, ScoutRifle as scr, SMG as smg, Sword as sw, Grenade as gr, Rocket as ro, Machine as ma
->>>>>>> 2e21f2cff66bb50ffcf7204bc2605f36b71d9e8b
 
-newWeapon = pr.PulseRifle ()
+
+
+newWeapon = pr.PulseRifle ("hi")
 newWeapon1 = hc.HandCannon ()
 newWeapon2 = ar.AutoRifle ()
 newWeapon3 = scr.ScoutRifle ()
@@ -25,7 +12,10 @@ newWeapon5 = sw.Sword ()
 newWeapon6 = gr.Grenade ()
 newWeapon7 = ro.Rocket()
 newWeapon8 = ma.Machine()
-
+newWeapon9 = sr.Sniper ()
+newWeapon10 = sa.Sidearm ()
+newWeapon11 = sg.Shotgun ()
+newWeapon12 = fr.Fusion ()
 
 
 
@@ -38,6 +28,9 @@ print (newWeapon5.gun)
 print (newWeapon6.gun)
 print (newWeapon7.gun)
 print (newWeapon8.gun)
+print (newWeapon9.gun)
+print (newWeapon10.gun)
+print (newWeapon11.gun)
+print (newWeapon12.gun)
 
 
->>>>>>> ed6a66f5cb1c7613db5d4b496909faa3fd4ffd22
